77b652be2a299d77517468ea7a4c4c0143b97d18
Jekyns <jekyns@mail.ru>
2019-05-13 15:28:12 +0300

    Update

--- OrmTask.rb
+++ OrmTask.rb
@@ -3,29 +3,46 @@ require 'pg'
 class Model
    protected
    @bd
+
    public
 
    def initialize (params)
-     @bd=PG::Connection.open(dbname:'test')
-
+    @bd=PG::Connection.open(dbname:'test')
     @params=params
     make_good_strings
      #@bd.exec(‘CREATE TABLE public.“Test1”(id SERIAL PRIMARY KEY ,Name text, Surname text,Age int);’)
    end
 
-   def create()
-     @bd.exec('INSERT INTO public."'+self.class.name+"\"(#{@keys})"+"VALUES (#{@values});")
-   end
-   def read()
+   def read_all
      rows=@bd.exec('SELECT * from public."'+self.class.name+'" order by id asc')
      rows.each{|row| puts row}
      puts "-----------"
    end
+
+   def create()
+    if @id.nil?
+     @res=@bd.exec('INSERT INTO public."'+self.class.name+"\"(#{@keys})"+"VALUES (#{@values}) RETURNING id;")
+     @id = @res.first['id']
+    else
+    puts "Запись уже существует"
+    end
+   end
+   def read()
+    unless @id.nil?
+     rows=@bd.exec('SELECT * from public."'+self.class.name+"\" where id=#{@id} order by id asc")
+     rows.each{|row| puts row}
+     puts "-----------"
+
+    else
+      puts "запись не создана"
+    end
+   end
    def update(pk)
      @bd.exec('update public."'+self.class.name+'" set'+ "#{@update_str} where id="+pk.to_s)
    end
-   def delete(pk)
-     @bd.exec('delete from public."Test1"'+ "where id="+pk.to_s)
+   def delete()
+     @bd.exec('delete from public."Test1"'+ "where id="+@id.to_s)
+     @id=nil
    end
 
    def make_good_strings
@@ -51,6 +68,7 @@ end
 
 
 class Test1<Model
+  attr_reader :id
   attr_accessor :name,:surname,:age
   def initialize(params)
     @name=params[:name]
@@ -58,14 +76,16 @@ class Test1<Model
     @age=params[:age]
     super
   end
+end
 
-
+class Test2<Model
 end
 
 #model1=Model.new;
 t1=Test1.new(name:"JustName",surname:"Null",age:40)
+t1.create
 t1.read
-#t1.create
-t1.update(43)
-#t1.delete(46)
-t1.read
+#t1.update(43)
+t1.delete
+t1.read_all
+
