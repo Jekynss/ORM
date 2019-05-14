696d12ff7fc0bc7b2cc8dc1a19935a79dd86b8e2
Jekyns <jekyns@mail.ru>
2019-05-14 13:54:15 +0300

    Update

--- OrmTask.rb
+++ OrmTask.rb
@@ -1,157 +1,139 @@
 require 'pg'
 
-$BD=PG::Connection.open(dbname:'test')
+$BD = PG::Connection.open(dbname: 'test')
+
 class ApplicationRecord
-   protected
-   #$BD
 
-   public
-   def initialize ()
-    #$BD=PG::Connection.open(dbname:'test')
-    @table_name='public."'+self.class.name+'"'
-    #make_good_strings
-    #$BD.exec('CREATE TABLE public."Test1"(id SERIAL PRIMARY KEY ,Name text, Surname text,Age int);')
-   end
+  def initialize
+    @table_name = "public.\"#{self.class.name}s\""
+  end
 
-   def  read_all
-       rows=$BD.exec('SELECT * from '+@table_name+' order by id asc')
-     rows.each{|row| puts row}
-     puts "-----------"
-   end
-
-   def create()
+  def create
     if @id.nil?
-     make_good_strings
-     @res=$BD.exec('INSERT INTO '+@table_name+" (#{@keys})"+"VALUES (#{@values}) RETURNING id;")
-     @id = @res.first['id']
+      make_good_strings
+      @res = $BD.exec("INSERT INTO #{@table_name} (#{@keys}) VALUES (#{@values}) RETURNING id;")
+      @id = @res.first['id']
     else
-    puts "Запись уже создана"
+      puts "Запись уже существует"
     end
-   end
-   def read()
-    unless @id.nil?
-        rows=$BD.exec('SELECT * from '+@table_name+" where id=#{@id} order by id asc")
-     rows.each{|row| puts row}
-     puts "-----------"+$BDD.to_s
+  end
 
+  def read
+    unless @id.nil?
+      rows = $BD.exec("SELECT * from #{@table_name} where id = #{@id} order by id asc")
+      rows.each{ |row| puts row }
+      puts "-----------"
     else
-      puts "Запись еще не создана"
+      puts "Запись еще не существует"
     end
-   end
-   def update()
+  end
+
+  def update
     make_good_strings
     unless @id.nil?
-        $BD.exec('update '+@table_name+' set'+ "#{@update_str} where id="+@id)
+      $BD.exec("update #{@table_name} set #{@update_str} where id = #{@id}")
     else
-    puts "Запись еще не создана"
+      puts "Запись еще не существует"
     end 
-   end
-   def delete()
+  end
+
+  def delete
     unless @id.nil?
-     $BD.exec('delete from '+@table_name+"where id="+@id.to_s)
-     @id=nil
+      $BD.exec("delete from #{@table_name} where id = #{@id.to_s}")
+      @id = nil
     else
-      puts "запись не создана"
+      puts "запись не существует"
     end
-   end
-   def make_good_strings(params=@params)
-    @values=""
-    @keys=""
-    @update_str=""
+  end
+
+  def make_good_strings(params = @params)
+    @values = ""
+    @keys = ""
+    @update_str = ""
 
 
     params.values.each do |elem| 
       if elem.is_a?String
-        @values+="\'#{elem}\',"
+        @values+= "\'#{elem}\',"
       else
-        @values+=elem.to_s+","
+        @values+= "#{elem},"
       end
     end
-    params.keys.each{|elem| @keys+=elem.to_s+","}
-    @keys[@keys.length-1]=""
-    @values[@values.length-1]=""
+    params.keys.each{|elem| @keys+= "#{elem},"}
+    @keys[@keys.length-1] = ""
+    @values[@values.length-1] = ""
 
-    params.keys.each{|elem| @update_str+=" #{elem}=#{@values.split(',')[params.keys.index elem]}, "}
-    @update_str[@update_str.length-2]=""
-   end
+    params.keys.each{|elem| @update_str+= " #{elem} = #{@values.split(',')[params.keys.index elem]}, "}
+    @update_str[@update_str.length-2] = ""
+  end
 end
 
 
-class Test1<ApplicationRecord
+class Book<ApplicationRecord
   attr_reader :id
   attr_accessor :name,:surname,:age
   def initialize(params)
-    @params=params
-    @name=params[:name]
-    @surname=params[:surname]
-    @age=params[:age]
-    @id=params.values[3]
+    @params = params
+    @name = params[:name]
+    @surname = params[:surname]
+    @age = params[:age]
+    @id = params.values[3]
     super()
   end
   def name=(value)
-    @name=value
-    @params[:name]=@name
+    @name = value
+    @params[:name] = @name
   end
   def surname=(value)
-    @surname=value
-    @params[:surname]=@surname
+    @surname = value
+    @params[:surname] = @surname
   end
   def age=(value)
-    @age=value
-    @params[:age]=@age
+    @age = value
+    @params[:age] = @age
   end
   def self.read_all
-     rows=$BD.exec('SELECT * from public."Test1" order by id asc')
-     rows.each{|row| puts row}
-     puts "-----------"
+    rows = $BD.exec('SELECT * from public."Books" order by id asc')
+    rows.each{|row| puts row}
+    puts "-----------"
   end
 
   def self.find_by(param)
-     res=$BD.exec("SELECT * from public.\"Test1\" where #{param.keys[0]}='#{param.values[0]}' order by id asc")
-     retObj=Test1.new(res[0].keys[1].to_sym=>res[0].values[1].to_s,res[0].keys[2].to_sym=>res[0].values[2].to_s,res[0].keys[3].to_sym=>res[0].values[3].to_s,res[0].keys[0].to_sym=>res[0].values[0].to_s)
-
+     res = $BD.exec("SELECT * from public.\"Books\" where #{param.keys[0]} = '#{param.values[0]}' order by id asc")
+     retObj = Book.new(res[0].keys[1].to_sym => res[0].values[1].to_s,res[0].keys[2].to_sym => res[0].values[2].to_s,res[0].keys[3].to_sym => res[0].values[3].to_s,res[0].keys[0].to_sym => res[0].values[0].to_s)
   end
+
 end
 
-class Test2<ApplicationRecord
+class User<ApplicationRecord
   attr_reader :id
   attr_accessor :name,:age
   def initialize(params)
-    @params=params
+    @params = params
     super()
   end
 
   def name=(value)
-    @name=value
-    @params[:name]=@name
+    @name = value
+    @params[:name] = @name
   end
+
   def age=(value)
-    @age=value
-    @params[:age]=@age
+    @age = value
+    @params[:age] = @age
   end
+
   def self.read_all
-     rows=$BD.exec('SELECT * from public."Test2 " order by id asc')
+     rows = $BD.exec('SELECT * from public."Users" order by id asc')
      rows.each{|row| puts row}
      puts "-----------"
   end
+
 end
 
-#model1=ApplicationRecord.new;
-t1=Test1.new(name:"JustName",surname:"Null",age:40)
-t1.name="Text"
-#t1.create
+t1 = Book.new(name:"JustName",surname:"Null",age:40)
+t1.name = "Tett"
+t1.create
 t1.read
-t1.name="Test"
-t1.age=100
-t1.update()
-t1.read
-#t1.delete
-tobj=Test1.find_by(age:30)
-tobj.read
-
-Test1.read_all
-#t2=Test2.new(name:"NewTest",age:1)
-#t2.create
-#t2.read
-#Test2.read_all
+Book.read_all
 
